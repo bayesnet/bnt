acf13ccd7ee6add0f385bd096ad78e781c70713e
Sacha Gunaratne <sacha.gunaratne@gmail.com>
2017-09-27 18:43:51 -0400

    Added update_ess.m function which will update the ESS for the Von Mises Dist. Currently missing the correct likelihood calculations

---
+++ BNT/CPDs/@vonMises_CPD/update_ess.m
@@ -0,0 +1,35 @@
+function CPD = update_ess(CPD, fmarginal, evidence, ns, cnodes, hidden_bitv)
+% UPDATE_ESS Update the Expected Sufficient Statistics of a Von Mises node
+% function CPD = update_ess(CPD, fmarginal, evidence, ns, cnodes, hidden_bitv)
+
+%if nargin < 6
+%  hidden_bitv = zeros(1, max(fmarginal.domain));
+%  hidden_bitv(find(isempty(evidence)))=1;
+%end
+
+dom = fmarginal.domain;
+ps = dom(1:end-1);
+cps = myintersect(ps, cnodes);
+
+CPD.nsamples = CPD.nsamples + 1;            
+[ss cpsz dpsz] = size(CPD.weights); % ss = self size
+[ss dpsz] = size(CPD.mean);
+
+% general (non-vectorized) case
+fullm = add_evidence_to_gmarginal(fmarginal, evidence, ns, cnodes); % slow!
+
+if dpsz == 1 % no discrete parents
+  w = 1;
+else
+  w = fullm.T(:); % what is T?
+end
+
+CPD.Wsum = CPD.Wsum + w;
+yi = (cpsz+1):(cpsz+ss);
+for i=1:dpsz
+  muY = fullm.mu(yi, i);
+  SYY = fullm.con(yi, yi, i);
+  CPD.WYsum(:,i) = CPD.WYsum(:,i) + w(i)*muY; % is this correct for Von Mises. I don't think so 
+  CPD.WYYsum(:,:,i) = CPD.WYYsum(:,:,i) + w(i)*(SYY + muY*muY'); % E[X Y] = Cov[X,Y] + E[X] E[Y]
+end                
+
