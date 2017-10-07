9d4e6a48ae9e566a4a276c7052c23addaf6c396b
Sacha Gunaratne <sacha.gunaratne@gmail.com>
2017-10-07 17:41:54 -0400

    Added w as an output in all the if statements to ensure complete output

--- BNT/CPDs/@vonMises_CPD/vonMises_CPD_params_given_dps.m
+++ BNT/CPDs/@vonMises_CPD/vonMises_CPD_params_given_dps.m
@@ -1,11 +1,12 @@
 function [m,k,w] = vonMises_CPD_params_given_dps(CPD,domain,evidence)
 %VONMISES_ Summary of this function goes here
 %   Detailed explanation goes here
-    ps = domain(1:end-1);
+ps = domain(1:end-1);
 dps = ps(CPD.dps);
 if isempty(dps)
   m = CPD.mean;
   k = CPD.con;
+  w = CPD.weights;
 else
   odom = domain(~isemptycell(evidence(domain)));
   dops = myintersect(dps, odom);
@@ -14,7 +15,8 @@ else
     dpsizes = CPD.sizes(CPD.dps);
     dpval = subv2ind(dpsizes, dpvals(:)');
     m = CPD.mean(:, dpval);
-    k = CPD.con(:, dpval);
+    k = CPD.con(:, :, dpval);
+    w = CPD.weights(:,:,dpval);
   else
     map = find_equiv_posns(dops, dps);
     index = mk_multi_index(length(dps), map, dpvals);
