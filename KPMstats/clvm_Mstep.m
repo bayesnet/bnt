51daaa6833e630fbe11fa1fa0ad9b93de2454028
Sacha Gunaratne <sacha.gunaratne@gmail.com>
2018-01-15 17:16:34 -0500

    More documentation

--- KPMstats/clvm_Mstep.m
+++ KPMstats/clvm_Mstep.m
@@ -1,39 +1,17 @@
 function [mu, con, B] = clvm_Mstep(w, Y, YY, YTY, X, XX, XY, varargin)
-% MSTEP_CLG Compute ML/MAP estimates for a conditional linear Gaussian
-% [mu, Sigma, B] = Mstep_clg(w, Y, YY, YTY, X, XX, XY, varargin)
-%
-% We fit P(Y|X,Q=i) = N(Y; B_i X + mu_i, Sigma_i) 
-% and w(i,t) = p(M(t)=i|y(t)) = posterior responsibility
-% See www.ai.mit.edu/~murphyk/Papers/learncg.pdf.
-%
-% See process_options for how to specify the input arguments.
-%
+% MSTEP_CLG Compute ML/MAP estimates for a conditional linear Von Mises
+% [mu, Con, B] = clvm_Mstep(w, Y, YY, YTY, X, XX, XY, varargin)
+
+% This currently only accounts for conditions where there are no cts parents. 
+% Dsc-->Cts is the structure that this can handle. 
+
 % INPUTS:
 % w(i) = sum_t w(i,t) = responsibilities for each mixture component
 %  If there is only one mixture component (i.e., Q does not exist),
 %  then w(i) = N = nsamples,  and 
 %  all references to i can be replaced by 1.
-% Y(:,i) = sum_t w(i,t) y(:,t) = weighted observations
-% YY(:,:,i) = sum_t w(i,t) y(:,t) y(:,t)' = weighted outer product
-% YTY(i) = sum_t w(i,t) y(:,t)' y(:,t) = weighted inner product
-%   You only need to pass in YTY if Sigma is to be estimated as spherical.
-%
-% In the regression context, we must also pass in the following
-% X(:,i) = sum_t w(i,t) x(:,t) = weighted inputs
-% XX(:,:,i) = sum_t w(i,t) x(:,t) x(:,t)' = weighted outer product
-% XY(i) = sum_t w(i,t) x(:,t) y(:,t)' = weighted outer product
-%
-% Optional inputs (default values in [])
-%
-% 'cov_type' - 'full', 'diag' or 'spherical' ['full']
-% 'tied_cov' - 1 (Sigma) or 0 (Sigma_i) [0]
-% 'clamped_cov' - pass in clamped value, or [] if unclamped [ [] ]
-% 'clamped_mean' - pass in clamped value, or [] if unclamped [ [] ]
-% 'clamped_weights' - pass in clamped value, or [] if unclamped [ [] ]
-% 'cov_prior' - added to Sigma(:,:,i) to ensure psd [0.01*eye(d,d,Q)]
-%
-% If cov is tied, Sigma has size d*d.
-% But diagonal and spherical covariances are represented in full size.
+% Y(:,i) = sum_t w(i,t) cos(y(:,t)) = weighted observations
+% YY(:,:,i) = sum_t w(i,t) sin(y(:,t)) = weighted outer product
 
 [cov_type, tied_cov, ...
  clamped_cov, clamped_mean, clamped_weights,  cov_prior, ...
